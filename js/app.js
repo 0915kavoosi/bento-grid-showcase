7314166f8ea97cc0ed99df17e2c74c77cd7cc11f
Milad <kavoosim71@gmail.com>
2026-02-06 22:46:28 +0330

    Fix: resolve layout issues and improve alignment

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// const btn = document.querySelector('.menu-btn');
+// const menu = document.querySelector('.menu');
+
+
+btn.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
+
+let x = 5;
+let y = 2026
+let sum = x + y;
+// x = parseInt(x);
+console.log(sum);
+
+
+
+
+s = 5;
+function mttest (){
+
+}
+
+
